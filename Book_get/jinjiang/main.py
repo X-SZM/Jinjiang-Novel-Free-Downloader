bb6a08f79256e0d807194302bb11f8d24eab8cef
X-SZM <55021001+X-SZM@users.noreply.github.com>
2022-11-29 09:15:03 +0800

    Delete main.py

--- Book_get/jinjiang/main.py
+++
@@ -1,6 +0,0 @@
-# -*- coding:utf-8 -*-
-from html_get import HtmlGet
-print("========X-SZM晋江免费文下载 Ver.beta_0.0.1=======")
-input_url = input("请从浏览器地址栏复制晋江小说文章主页地址:>")
-HtmlGet(input_url)
-print("Task is done.")
